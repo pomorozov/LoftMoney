a434f7e8723ebf7690748e3976f339f9136106a5
pomorozov <morozov.p.ub@gmail.com>
2020-10-20 21:11:31 +1200

    stage DZ_1 Saturday

--- app/src/main/java/com/pmorozov/loftmoney/MainActivity.java
+++ app/src/main/java/com/pmorozov/loftmoney/MainActivity.java
@@ -2,7 +2,9 @@ package com.pmorozov.loftmoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnClick).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent newActivity=new Intent(getApplicationContext(), AddItemActivity.class);
+                startActivity(newActivity);
+            }
+        });
         //TODO: -Here we will work with recyclerview
     }
 }
